ef27516287b79aa7857c3b2286fd5031dbd2a611
Yoga C. Pranata <yoga.pranata@efishery.com>
2023-12-04 17:55:04 +0700

    fix(fragment): Fix crash `Can't access the Fragment View's LifecycleOwner when getView() is null` when checking the fragment lifecycle using `lifecycle.currentState.isAtLeast(Lifecycle.State.INITIALIZED)`

--- android-viewbinding/src/main/java/android/viewbinding/library/fragment/FragmentBinding.kt
+++ android-viewbinding/src/main/java/android/viewbinding/library/fragment/FragmentBinding.kt
@@ -5,12 +5,13 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
 import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
 /**
- * Updated by Yoga C. Pranata on 06/05/2021.
+ * Updated by Yoga C. Pranata on 04/12/2023.
  * Android Engineer
  *
  * How to use:
@@ -29,52 +30,59 @@ class FragmentViewBindingDelegate<T : ViewBinding>(
     private val fragment: Fragment
 ) : ReadOnlyProperty<Fragment, T> {
 
+
     /**
      * initiate variable for binding view
      */
     private var binding: T? = null
+
 
     /**
      * get the bind method from View class
      */
     private val bindMethod = bindingClass.getMethod("bind", View::class.java)
 
-    @Suppress("UNCHECKED_CAST")
-    override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
-        binding?.let { return it }
 
-        /**
-         * Adding observer to the fragment lifecycle
-         */
+    /**
+     * Adding observer to the fragment lifecycle
+     */
+    init {
         fragment.lifecycle.addObserver(object : DefaultLifecycleObserver {
-            override fun onCreate(owner: LifecycleOwner) {
-                fragment.viewLifecycleOwnerLiveData.observe(fragment) { viewLifecycleOwner ->
-                    viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
-                        override fun onDestroy(owner: LifecycleOwner) {
-                            /**
-                             * Clear the binding when Fragment lifecycle called the onDestroy
-                             */
-                            binding = null
-                        }
-                    })
+            val viewLifecycleOwnerObserver = Observer<LifecycleOwner?> { owner ->
+                if (owner == null) {
+                    binding = null
                 }
             }
+            override fun onCreate(owner: LifecycleOwner) {
+                fragment.viewLifecycleOwnerLiveData.observeForever(viewLifecycleOwnerObserver)
+            }
+
+            override fun onDestroy(owner: LifecycleOwner) {
+                fragment.viewLifecycleOwnerLiveData.removeObserver(viewLifecycleOwnerObserver)
+            }
         })
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
+
+        binding?.let {
+            if(it.root === thisRef.view) {
+                return it
+            }
+        }
 
 
         /**
-         * Checking the fragment lifecycle
+         * Checking the fragment's view
          */
-        val lifecycle = fragment.viewLifecycleOwner.lifecycle
-        if (!lifecycle.currentState.isAtLeast(Lifecycle.State.INITIALIZED)) {
-            error("Cannot access view bindings. View lifecycle is ${lifecycle.currentState}!")
-        }
+        val view = thisRef.view ?: error("Should not attempt to get bindings when the Fragment's view is null.")
 
 
         /**
          * Bind layout
          */
-        val invoke = bindMethod.invoke(null, thisRef.requireView()) as T
+        val invoke = bindMethod.invoke(null, view) as T
 
         return invoke.also { this.binding = it }
     }
